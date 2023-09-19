30f4a421577a4ec0701cc2c517495f1f0e6f0bb7
Git-abby <malekabrar56@gmail.com>
2023-09-19 19:02:11 -0400

    0919

--- aqua-mart/src/App.js
+++ aqua-mart/src/App.js
@@ -6,18 +6,43 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import { Routes, Route } from "react-router-dom";
 import Login from "./components/login/Login";
 import Register from "./components/register/Register";
-import Home from "./components/home/Home"
+import Home from "./components/home/Home";
 import Footer from "./components/footer/Footer";
+import ImageSlider from "./components/home/ImageSlider";
 
 function App() {
   return (
     <div className="app">
       <Router>
-      <Routes>
-        <Route path="/" element={<><Home /><Footer/></>} />
-        <Route path="/login" element={<><Login /><Footer /></>} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
+        <Routes>
+          <Route
+            path="/image"
+            element={
+              <>
+            <ImageSlider />
+              </>
+            }
+          />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/login"
+            element={
+              <>
+                <Login />
+                <Footer />
+              </>
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <>
+                <Home />
+                <Footer />
+              </>
+            }
+          />
+        </Routes>
       </Router>
     </div>
   );
--- aqua-mart/src/components/home/Home.js
+++ aqua-mart/src/components/home/Home.js
@@ -1,12 +1,32 @@
-import React from 'react'
-import '../home/home.css';
+import React from "react";
+import "../home/home.css";
+import home_img from "../../images/7368089.jpg";
+// import ImageSlider from "./ImageSlider";
 
 function Home() {
   return (
-    <div className='home'>
-        <h3>Home</h3>
+    <div className="home">
+      <div className="home__text">
+        <div className="home__text-inner">
+          <h1 className="home__text-title">This is Aqua-Mart</h1>
+          <p className="home__text-para">
+            Sell or Buy things Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+            esse cillum dolore eu fugiat nulla pariatur.{" "}
+          </p>
+          <button className="btn">Explore</button>
+        </div>
+      </div>
+      <div className="home__img">
+        <img src={home_img} alt="home_img" />
+      </div>
+
+      
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
