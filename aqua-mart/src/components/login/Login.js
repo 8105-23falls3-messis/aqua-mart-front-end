99ee867d2e5f9cc8187bd7dc2a847d630c51d3cc
Git-abby <malekabrar56@gmail.com>
2023-11-07 18:49:43 -0500

    Changed :: Header, Login :: Reduces

--- aqua-mart/src/components/login/Login.js
+++ aqua-mart/src/components/login/Login.js
@@ -10,29 +10,24 @@ import { useStateValue } from "../StateProvider";
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
- const [{user}, dispatch] = useStateValue();
+  const [{ user }, dispatch] = useStateValue();
 
   const navigate = useNavigate();
   // console.log(user, "Dispatch Function: ", dispatch);
   // console.log("USER NAME & PASSWORD ", email, password);
 
-
   //Login
   const handleLogin = async () => {
-    const { resData } = await AuthService.login(
-      email,
-      password,
-      1
-    );
+    const { resData } = await AuthService.login(email, password, 1);
 
     if (resData.success) {
       console.log(resData.token);
       console.log(resData.msg, resData.user, resData.usermail);
       // Update your app's state to indicate the user is logged in
-      dispatch({ type: "LOGIN", user: resData.user, token : resData.token});
+      dispatch({ type: "LOGIN", user: resData.user, token: resData.token });
       // const item = JSON.stringify(token[0][1]);
       // console.log(item);
-      navigate('/list');
+      navigate("/list");
 
       // Redirect to a protected route or perform other actions
     } else {
@@ -45,45 +40,48 @@ function Login() {
 
   return (
     <div className="login">
-
       <div className="login__wrapper">
         <div className="login__logo">
           <div className="logo">
             <img src={logo} alt="logo" />
           </div>
-          <p>Welcome to <span className="color_change">AQUAMART</span></p>
+          <p>
+            Welcome to <span className="color_change">AQUAMART</span>
+          </p>
         </div>
+        <form>
+          <div className="login__form">
+            <div className="login__input">
+              <PersonIcon className="icon" />
+              <input
+                aria-label="username"
+                type="text"
+                value={email}
+                placeholder="username"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="login__input">
+              <LockIcon className="icon" />
+              <input
+                aria-label="password"
+                type="password"
+                value={password}
+                placeholder="password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
 
-        <div className="login__form">
-          <div className="login__input">
-            <PersonIcon className="icon" />
-            <input
-              type="text"
-              value={email}
-              placeholder="username"
-              onChange={(e) => setEmail(e.target.value)}
-            />
+            {/* <Link className="cursor login-btn"> */}
+            <a onClick={handleLogin} className="cursor login-btn">Sign In</a>
+            {/* </Link> */}
+            <div className="dont_have_acc">
+              <Link to={"/register"}>
+                <p>Don't have an account?</p>
+              </Link>
+            </div>
           </div>
-          <div className="login__input">
-            <LockIcon className="icon" />
-            <input
-              type="password"
-              value={password}
-              placeholder="password"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-
-          <Link className="cursor login-btn">
-            <a className="cursor" onClick={handleLogin}>Sign In</a>
-          </Link>
-          <div className="dont_have_acc">
-            
-            <Link to={"/register"}>
-              <p>Don't have an account?</p>
-            </Link>
-          </div>
-        </div>
+        </form>
       </div>
     </div>
   );
